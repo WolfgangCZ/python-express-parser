c99bb0d5ed5ab72082a924b772a9e688530563c5
Wolgang <kralicek.roman@gmail.com>
2024-02-04 16:50:59 +0100

    lexer working

--- schema_parser.py
+++ schema_parser.py
@@ -11,7 +11,7 @@ sys.path.append(dirName)
 
 
 
-exp_file_path = f"{dirName}\\schema\\basic_schema.exp"
+exp_file_path = f"{dirName}\\schema\\ap242ed3.exp"
 
 with open(exp_file_path, "r", encoding="utf-8") as file:
     file_content = file.read()
@@ -22,60 +22,61 @@ class Lexer:
         self.content = content
         self.index = 0
         self.token = ''
-        self.delimiters = ["\n", " "]
-        self.operators = ['(', ')', '/', '*', '+', '-', '=']
+        self.delimiters = "\n "
+        self.operators = "()/*+-=!{}[|]<>?':;,.%$&#@"
 
     def grab_char(self):
         char = self.content[self.index]
         return char
 
-    def skip_chars(self, num: int = 1):
-        self.index += num
-
-    def look_char(self, num: int):
+    def lookup_char(self, num: int):
         return self.content[self.index+num]
 
-    def next_char(self):
-        self.index += 1
+    def next_char(self, num: int = 1):
+        self.index += num
 
-    def yield_token(self):
+    def yield_token(self, buff_char: str = ''):
         tmp = self.token
-        self.token = ""
+        self.token = buff_char
         return tmp
-
-# find all operators
-# find all delimiters
 
     def get_next_token(self):
         comment = False
+        char = ''
+        if self.token:
+            return self.yield_token()
+
         while self.index < len(self.content):
             char = self.grab_char()
-            if char == '(' and self.look_char(1) == '*':
-                self.skip_chars(2)
+            if char == '(' and self.lookup_char(1) == '*':
+                self.next_char(2)
                 comment = True
-            if char == '*' and self.look_char(1)  == ')':
-                self.skip_chars(2)
+                if self.token:
+                    return self.yield_token()
+                continue
+
+            elif char == '*' and self.lookup_char(1) == ')':
+                self.next_char(2)
                 comment = False
+                continue
+
             if comment:
                 self.next_char()
                 continue
-            if char == '\n':
+
+            if self.delimiters.find(char) != -1:
                 self.next_char()
                 if self.token:
                     return self.yield_token()
-                else:
-                    continue
-#            elif self.look_char(1) == ';':
-#                return self.yield_token()
-#            elif char == ';':
-#                self.next_char()
-#                return ';'
-            elif char == ' ':
+                continue
+
+            if self.operators.find(char) != -1:
                 self.next_char()
+                operator = self.operators[self.operators.find(char)]
                 if self.token:
-                    return self.yield_token()
-                else:
-                    continue
+                    return self.yield_token(operator)
+                return operator
+
             else:
                 self.token += char
                 self.next_char()
@@ -91,13 +92,23 @@ class Lexer:
         else:
             return token
 
+class TokenParser:
+    def __init__(self, tokens: list):
+        self.tokens = tokens
+
+    def get_tokens(self):
+        pass
+
+    def 
+    
+    
 lexer = Lexer(file_content)
 print(file_content)
-
-
+counter = 1
+output = open("output.exp", "w", encoding="utf-8")
 for token in lexer:
-    print(f"token: {token}")
-
+    output.writelines(f"{counter}: {token}\n")
+    counter += 1
 # diferentiate between initialization and reference
 # space ' '
 # delimiter ';'
